e5b90208d56f02dbaf481ea295fc540ac7242b76
BenB1116 <bburns171@gmail.com>
2023-02-04 19:17:52 -0600

    Created basic structure

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///book.db'
+db = SQLAlchemy(app)
+
+class checkouts(db.model):
+    patron_id = db.Column(db.Integer, primary_key=True)
+    item_id = db.Column(db.Integer, primary_key=True)
+    count = db.Column(db.Integer)
+
+
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
