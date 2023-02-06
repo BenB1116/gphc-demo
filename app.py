b9e73950ab445a24bb644b5d41d1966b8d41d94b
BenB1116 <bburns171@gmail.com>
2023-02-05 20:32:47 -0600

    Added index column

--- app.py
+++ app.py
@@ -1,24 +1,28 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
 
 # Create and configure the app
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///library.db'
+
 db = SQLAlchemy(app)
 
 app.app_context().push()
 
 # Create the database schema
 class Checkout(db.Model):
-    patron_id = db.Column(db.Integer, primary_key=True, nullable = False)
-    item_id = db.Column(db.Integer, primary_key=True, nullable = False)
+    index = db.Column(db.Integer, primary_key=True)
+    patron_id = db.Column(db.Integer, nullable=False)
+    item_id = db.Column(db.Integer, nullable=False)
+
 
 class Inventory(db.Model):
-    item_id = db.Column(db.Integer, primary_key=True, nullable = False)
-    title = db.Column(db.String(100), nullable = False)
-    author_last = db.Column(db.String(100), nullable = False)
-    author_first = db.Column(db.String(100), nullable = False)
+    item_id = db.Column(db.Integer, primary_key=True, nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    author_last = db.Column(db.String(100), nullable=False)
+    author_first = db.Column(db.String(100), nullable=False)
+
 
 # Fill the database with data from csvs
 with open(r'C:\Users\Ben\Desktop\hrp-machine-learning\data\clean\patron_data.csv', 'r') as file:
@@ -27,7 +31,7 @@ patron_df.to_sql('Checkout', con=db.engine, if_exists='replace')
 
 with open(r'C:\Users\Ben\Desktop\hrp-machine-learning\data\clean\inv_data.csv', 'r') as file:
     inv_df = pd.read_csv(file)
-inv_df.to_sql('Inventory', index = False, con=db.engine, if_exists='replace')
+inv_df.to_sql('Inventory', index=False, con=db.engine, if_exists='replace')
 
 
 @app.route('/')
