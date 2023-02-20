f978a01de2162ed97ea5de1f5cee8b18d929deba
BenB1116 <bburns171@gmail.com>
2023-02-19 21:41:21 -0600

    Added comments and returns closest elements

--- knn.py
+++ knn.py
@@ -1,15 +1,16 @@
 import pandas as pd
-from itertools import chain
+from statistics import mean
 from collections import defaultdict
 
 class knn:
-    def __init__(self, checkout_df, k) -> None:
+    def __init__(self, checkout_df, k, n) -> None:
         self.checkout_df = checkout_df
         self.k = k
+        self.n = n
 
 
     def jaccard(self, item_set1 = set(), item_set2 = set()):
-        # 
+        # Calculate the numerator and denomenator for Jaccard similarity
         num = len(item_set1.intersection(item_set2))
         den = len(item_set1.union(item_set2))
 
@@ -20,12 +21,15 @@ class knn:
     
 
     def get_jaccard_score(self, item1, item2):
+        # Gather the patrons that have read item1
         item1_df = self.checkout_df[self.checkout_df['item_id'] == item1]
         item1_patrons = set(item1_df['patron_id'].values.tolist())
 
+        # Gather the patrons that have read item2
         item2_df = self.checkout_df[self.checkout_df['item_id'] == item2]
         item2_patrons = set(item2_df['patron_id'].values.tolist())
 
+        # Calculate the Jaccard similarity
         return self.jaccard(item1_patrons, item2_patrons)
 
 
@@ -34,7 +38,7 @@ class knn:
         can_df = self.checkout_df[self.checkout_df['item_id'] == item]
         can_patrons = set(can_df['patron_id'].values.tolist())
 
-        # Aggregate all items that the list fo patrons have read
+        # Aggregate all items that the list of patrons have read
         can_items = set()
         for patron in can_patrons:
             can_df = self.checkout_df[self.checkout_df['patron_id'] == patron]
@@ -44,26 +48,46 @@ class knn:
     
 
     def gen_sim_dict(self, item):
+        # Get a list of canidate patrons
         can_patrons = self.get_canidates(item)
 
+        # Compuer the simlarity between every item in the canidate item list
         sim_dict = {item2: self.get_jaccard_score(item, item2) for item2 in can_patrons}
         return sim_dict
     
 
-    def k_average_dicts(self, item_list):
+    def average_dicts(self, item_list):
         dict_list = []
+        # Generate the similarity dictionary for every item in the item list
         for item in item_list:
             dict_list.append(self.gen_sim_dict(item))
 
+        # Combine all simlarity dictionaries into one dictionary
         merged_dict = defaultdict(list)
         for d in dict_list:
             for key, value in d.items():
                 merged_dict[key].append(value)
 
+        # Take the k-average over the merged dictionary
         for key, value in merged_dict.items():
-            merged_dict[key].sort()
+            temp = merged_dict[key]
+            # Sort each list in descending order
+            temp.sort(reverse=True)
+            # Add zeroes up to k or truncate at k
+            merged_dict[key] = mean(temp[:self.k] + [0 for _ in range(self.k - len(temp))])
 
         return merged_dict
+    
+    def top_n_closests(self, item_list):
+        average_dict = self.average_dicts(item_list)
+        # Sort the dictinary by values
+        average_dict = {k: v for k, v in sorted(average_dict.items(), key=lambda item: item[1], reverse=True)}
+
+        # Remove the elements that are in item_list
+        top_list = set(average_dict.keys()) - set(item_list)
+
+        # Return the first n elements
+        return list(top_list)[:self.n]
 
         
         
@@ -71,7 +95,7 @@ class knn:
 
 
 patron_df = pd.read_csv('data\clean\patron_data.csv')
-new_knn = knn(patron_df, 6)
+new_knn = knn(patron_df, 3, 5)
 
 # print(new_knn.get_canidates(3))
 
@@ -83,4 +107,6 @@ new_knn = knn(patron_df, 6)
 
 # print(new_knn.merge_dicts({"1":3, "2":3, "4":5},{"1":3, "2":4, "5":5}))
 
-print(new_knn.combine_dicts([1, 2, 4]))
+# print(new_knn.average_dicts([1, 2, 4, 5, 6, 7]))
+
+print(new_knn.top_n_closests([224, 236, 714, 730]))
